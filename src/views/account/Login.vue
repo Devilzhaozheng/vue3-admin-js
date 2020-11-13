41a226ed070f5a061e8c878fec11e90541f8e2d7
xm313115 <wx_88a4a806713e4d94ae3a92b8b9667134@git.code.tencent.com>
2020-11-13 09:55:23 +0800

    test

--- src/views/account/Login.vue
+++ src/views/account/Login.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
+  <div class="login">
     登录页
+    <a-button type="primary">按钮</a-button>
   </div>
 </template>
 
@@ -11,5 +12,9 @@ name: "Login"
 </script>
 
 <style scoped>
-
+  .login {
+    background-color: #42b983;
+    width: 100vw;
+    height: 100vh;
+  }
 </style>
