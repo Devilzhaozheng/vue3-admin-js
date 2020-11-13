65cba9d8919c50d013aad8b4764d9db64b82e5e8
xm313115 <wx_88a4a806713e4d94ae3a92b8b9667134@git.code.tencent.com>
2020-11-13 17:24:45 +0800

    --

--- src/views/account/Login.vue
+++ src/views/account/Login.vue
@@ -1,12 +1,43 @@
 <template>
   <div class="login">
-    <div class="form-wrap"></div>
+    <div class="form-wrap">
+      <a-form
+        label-align="left"
+      >
+        <a-form-item label="用户名">
+          <a-input v-model:value="formConfig.userName" type="text" />
+        </a-form-item>
+        <a-form-item label="密码">
+          <a-input v-model:value="formConfig.userPassword" type="password" />
+        </a-form-item>
+        <a-form-item>
+          <a-button @click="login">
+            Submit
+          </a-button>
+        </a-form-item>
+      </a-form>
+      <div>{{ formConfig.userName }}----{{ formConfig.userPassword}}</div>
+    </div>
   </div>
 </template>
 
 <script>
+import { reactive } from "vue";
 export default {
-name: "Login"
+  name: "Login",
+  setup(props) {
+    const formConfig = reactive({
+      userName: null,
+      userPassword: null
+    })
+    const login = () => {
+      console.log(formConfig.userName, formConfig.userPassword);
+    }
+    return {
+      formConfig,
+      login
+    };
+  }
 }
 </script>
 
